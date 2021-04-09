d29162fe859ae79d49a1716b2fe0763569bbe28a
kien@kien <kien.nguyen@sotatek.com>
2021-04-09 10:00:00 +0700

    login-page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './Routes/commom-page/HomePage';
+import LoginPage from './component/login/LoginPage';
 
 function App() {
   return (
-    <HomePage/>
+    <LoginPage/>
   );
 }
 
